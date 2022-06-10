83f1714a6ea104450e320303b5f285341e481b74
Armando Rivera <rivera.armando997@gmail.com>
2022-06-09 23:59:07 -0500

    interval

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const puppeteer = require("puppeteer");
 const url = "https://www.vocaltraining.com.mx/111-fernanda-alvarez";
 
 const buttonVote = "#nc-vote-button-111";
-(async () => {
+const hour = 1000 * 60 * 61;
+
+const myFunc = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
   // click button
   await page.click(buttonVote);
   await page.screenshot({ path: "example.png" });
-  console.log("object");
   await page.evaluate((e) => {
     //do something on the page
   });
-})();
+  // close browser
+  await browser.close();
+};
+
+myFunc();
+
+setInterval(myFunc, hour);
